dbad95f7a940efdfb205e9fd226ea9efe3914174
romulofull <romulo.castro@gmail.com>
2024-07-31 17:00:29 -0500

    Metodo Shift

--- js/Frutas.js
+++ js/Frutas.js
@@ -5,3 +5,8 @@ console.log (frutas);
 let paises = ["Ecuador", "Colombia", "Peru"];
 paises.pop();
 console.log (paises);
+
+let colores = ["amarillo", "azul", "rojo"];
+colores.shift();
+console.log (colores);
+
