0f05f426a2940de392e2e72f03c1275a7604291b
romulofull <romulo.castro@gmail.com>
2024-07-31 19:16:27 -0500

    Metodo Filter, Concat y Reverse

--- js/Frutas.js
+++ js/Frutas.js
@@ -13,3 +13,15 @@ console.log (colores);
 let ciudades = ["Madrid", "Barcelona", "Teruel"];
 ciudades.unshift("Munich");
 console.log (ciudades);
+
+let frutasconJ = frutas.filter(fruta => fruta.includes("j"));
+console.log (frutasconJ);
+
+let animales = ["jirafa","elefante","lobo"];
+let animales2 = ["aves","culebra","monos"];
+let todoslosAnimales = animales.concat(animales2);
+console.log (todoslosAnimales);
+
+let helados = ["chocolate","vainilla","limon", "fresa", "menta"];
+helados.reverse();
+console.log(helados);
